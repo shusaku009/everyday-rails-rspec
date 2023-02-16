1c349e6d46f024ac4943d64f4d3c54814cd4f09c
shusaku009 <shusaku009@gmail.com>
2023-02-16 16:27:29 +0900

    Initial Commit

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,15 @@ module Projects
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_spec: false,
+        routing_specs: false
+      g.factory_bot
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files

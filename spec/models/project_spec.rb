8f0dab63af68163d0cfb1fff77bfd78d007225ea
shusaku009 <shusaku009@gmail.com>
2023-02-22 08:52:54 +0900

    clean Factory Bot
    Factory Botを使ってリファクタリングをした

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,12 +3,13 @@ require "rails_helper"
 RSpec.describe Project, type: :model do
   # ユーザー単位では重複したプロジェクト名を許可しないこと
   it "dose not allow duplicate project names per user" do
-    user = User.create(
-      first_name: "Joe",
-      last_name: "Tester",
-      email: "joeester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-    )
+    user = FactoryBot.create(:user) 
+    # User.create(
+    #   first_name: "Joe",
+    #   last_name: "Tester",
+    #   email: "joeester@example.com",
+    #   password: "dottle-nouveau-pavilion-tights-furze",
+    # )
 
     user.projects.create(
       name: "Test Project",
@@ -24,23 +25,25 @@ RSpec.describe Project, type: :model do
   # 二人のユーザーが同じ名前を使うことは許可すること
   it "allows two users to 
   share a project name" do
-  user = User.create(
-    first_name: "Joe",
-    last_name: "Tester",
-    email: "Joetester@example.com",
-    password: "dottle-nouveau-pavilion-tights-furze",
-  )
+  user = FactoryBot.create(:user)
+  # user = User.create(
+  #   first_name: "Joe",
+  #   last_name: "Tester",
+  #   email: "Joetester@example.com",
+  #   password: "dottle-nouveau-pavilion-tights-furze",
+  # )
 
   user.projects.create(
     name: "Test Project",
   )
 
-  other_user = User.create(
-    first_name: "jane",
-    last_name: "Tester",
-    email: "janetester@example.com",
-    password: "dottle-nouveau-pavilion-tights-furze"
-  )
+  other_user = FactoryBot.create(:user)
+  # User.create(
+  #   first_name: "jane",
+  #   last_name: "Tester",
+  #   email: "janetester@example.com",
+  #   password: "dottle-nouveau-pavilion-tights-furze"
+  # )
 
   other_project = other_user.projects.build(
     name: "Test Project",

6c49798d4c8d6f15a3d5670454466f77e6a321cb
shusaku009 <shusaku009@gmail.com>
2023-02-25 11:07:45 +0900

    add tasks_controller_spec

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -16,4 +16,36 @@ RSpec.describe TasksController, type: :controller do
       expect(response.content_type).to include "application/json"
     end
   end
+
+  describe "create" do
+    # JSON形式でレスポンスを返すこと
+    it "responds with JSON formatted output" do
+      new_task = { name: "New test task" }
+      sign_in @user
+      post :create, format: :json,
+        params: { project_id: @project.id, task: new_task }
+      expect(response.content_type).to include "application/json"
+    end
+
+    # 新しいタスクをプロジェクトに追加すること
+    it "adds a new task to the porject" do
+      new_task = { name: "New test task" }
+      sign_in @user
+      expect {
+        post :create, format: :json,
+          params: { project_id: @project.id, task: new_task }
+      }.to change(@project.tasks, :count).by(1)
+    end
+
+    # 認証を要求すること
+    it "requires authentication" do
+      new_task = { name: "New test task" }
+      # ここではあえてログインしない...
+      expect {
+        post :create, format: :json,
+          params: { project_id: @project.id, task: new_task }
+      }.to_not change(@project.tasks, :count)
+      expect(response).to_not be_successful
+    end
+  end
 end

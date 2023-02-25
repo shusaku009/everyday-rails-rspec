45281cec35668b17ebc8b7e9da02c85a6a5a093a
shusaku009 <shusaku009@gmail.com>
2023-02-25 14:31:41 +0900

    add projects_controller_spec
    projects コントローラーにnewアクションのテストを追加

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -72,6 +72,44 @@ RSpec.describe ProjectsController, type: :controller do
     end
   end
 
+  describe "#new" do
+    # 認証済みのユーザーとして
+    context "as an authenticated user" do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+
+      # 正常にレスポンスを返すこと
+      it "responds successfully" do
+        sign_in @user
+        get :new
+        expect(response).to be_successful
+      end
+
+      # 200レスポンスを返すこと
+      it "returns a 200 response" do
+        sign_in @user
+        get :new
+        expect(response).to have_http_status "200"
+      end
+    end
+
+    # ゲストとして
+    context "as a guest" do
+      # 302レスポンスを返すこと
+      it "returns a 302 response" do
+        get :new
+        expect(response).to have_http_status "302"
+      end
+
+      # サインイン画面にリダイレクトすること
+      it "redirect to the sign-in page" do
+        get :new
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
+
   describe "#create" do
     # 認証済みのユーザーとして
     context "as an authenticated user" do

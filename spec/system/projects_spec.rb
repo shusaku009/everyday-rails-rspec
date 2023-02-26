d17f102c8e36b5517337874bc34b0cc266145e74
shusaku009 <shusaku009@gmail.com>
2023-02-26 11:23:40 +0900

    add system/projects_specific
    システムスペックのファイルを追加

---
+++ spec/system/projects_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "Projects", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  # ユーザーは新しいプロジェクトを作成する
+  scenario "user creates a new project" do
+    user = FactoryBot.create(:user)
+
+    visit root_path
+    click_link "Sign in"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+
+    expect {
+      click_link "New Project"
+      fill_in "Name", with: "Test Project"
+      fill_in "Description", with: "Trying out Capybara"
+      click_button "Create Project"
+
+      expect(page).to have_content "Project was successfully created"
+      expect(page).to have_content "Test Project"
+      expect(page).to have_content "Owner: #{user.name}"
+    }.to change(user.projects, :count).by(1)
+  end
+
+  scenario "guest adds a project" do
+    visit projects_path
+    click_link "New Project"
+  end
+end

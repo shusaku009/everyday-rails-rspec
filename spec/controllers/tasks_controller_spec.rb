c485a698c8fbec557c0c93c275566977a9a91102
shusaku009 <shusaku009@gmail.com>
2023-02-27 18:55:59 +0900

    clean shared_context
    shared_contextでリファクタリングをした

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,18 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
-  before do
-    @user = FactoryBot.create(:user)
-    @project = FactoryBot.create(:project, owner: @user)
-    @task = @project.tasks.create!(name: "Test task")
-  end
+  include_context "project setup"
 
   describe "#show" do
     # JSON形式でレスポンスを返すこと
     it "responds with JSON formatted output" do
-      sign_in @user
+      sign_in user
       get :show, format: :json,
-        params: { project_id: @project.id, id: @task.id }
+        params: { project_id: project.id, id: task.id }
       expect(response.content_type).to include "application/json"
     end
   end
@@ -21,20 +17,20 @@ RSpec.describe TasksController, type: :controller do
     # JSON形式でレスポンスを返すこと
     it "responds with JSON formatted output" do
       new_task = { name: "New test task" }
-      sign_in @user
+      sign_in user
       post :create, format: :json,
-        params: { project_id: @project.id, task: new_task }
+        params: { project_id: project.id, task: new_task }
       expect(response.content_type).to include "application/json"
     end
 
     # 新しいタスクをプロジェクトに追加すること
     it "adds a new task to the porject" do
       new_task = { name: "New test task" }
-      sign_in @user
+      sign_in user
       expect {
         post :create, format: :json,
-          params: { project_id: @project.id, task: new_task }
-      }.to change(@project.tasks, :count).by(1)
+          params: { project_id: project.id, task: new_task }
+      }.to change(project.tasks, :count).by(1)
     end
 
     # 認証を要求すること
@@ -43,8 +39,8 @@ RSpec.describe TasksController, type: :controller do
       # ここではあえてログインしない...
       expect {
         post :create, format: :json,
-          params: { project_id: @project.id, task: new_task }
-      }.to_not change(@project.tasks, :count)
+          params: { project_id: project.id, task: new_task }
+      }.to_not change(project.tasks, :count)
       expect(response).to_not be_successful
     end
   end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,25 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  before do
-    @user = User.create(
-      first_name: "Joe",
-      last_name: "Tester",
-      email: "joetester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-    )
-
-    @project = @user.projects.create(
-      name: "Test Project",
-    )
-  end
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project, owner: user) }
 
   # ユーザー、プロジェクト、メッセージがあれば有効な状態であること
   it "is valid with a user, project, and messsage" do
     note = Note.new(
       message: "This is a sample note.",
-      user: @user,
-      project: @project,
+      user: user,
+      project: project,
     )
     expect(note).to be_valid
   end
@@ -33,33 +23,44 @@ RSpec.describe Note, type: :model do
 
     # 文字列に一致するメッセージを検索する
   describe "search message for a term" do
-    before do
-      @note1 = @project.notes.create(
+    let!(:note1) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
         message: "This is the first note.",
-        user: @user,
       )
-      @note2 = @project.notes.create(
+    }
+    
+    let!(:note2) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
         message: "This is the second note.",
-        user: @user,
       )
-      @note3 = @project.notes.create(
+    }
+    
+    let!(:note3) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
         message: "First, preheat the oven.",
-        user: @user,
       )
-    end
+    }
     
     # 一致するデータが見つかるとき
     context "when a match is found" do
       # 検索文字列に一致するメモを返すこと
       it "returns notes that match the search term" do
-        expect(Note.search("first")).to include(@note1, @note3)
+        expect(Note.search("first")).to include(note1, note3)
       end
     end
   
     # 一致するデータが1件も見つからないとき
     context "when no match is found" do
+      # 空のコレクションを返すこと
       it "returns an empty collection" do
         expect(Note.search("message")).to be_empty
+        expect(Note.count).to eq 3
       end
     end
   end

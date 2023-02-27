9785fbf1486f659288b96d41c18641c7bd821a9a
shusaku009 <shusaku009@gmail.com>
2023-02-28 07:31:33 +0900

    add テストのカスタムマッチャを作成
    content_typeのカスタムマッチャを作成した

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe TasksController, type: :controller do
       sign_in user
       get :show, format: :json,
         params: { project_id: project.id, id: task.id }
-      expect(response.content_type).to include "application/json"
+      expect(response).to have_content_type :json
     end
   end
 
@@ -20,7 +20,7 @@ RSpec.describe TasksController, type: :controller do
       sign_in user
       post :create, format: :json,
         params: { project_id: project.id, task: new_task }
-      expect(response.content_type).to include "application/json"
+      expect(response).to have_content_type :json
     end
 
     # 新しいタスクをプロジェクトに追加すること

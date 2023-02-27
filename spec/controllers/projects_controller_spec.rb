07c65ce6f2e3ec9eb0dfdeae6a6bd32d0a746094
shusaku009 <shusaku009@gmail.com>
2023-02-28 08:21:16 +0900

    clean Rspecで失敗の集約をした

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -82,15 +82,11 @@ RSpec.describe ProjectsController, type: :controller do
       # 正常にレスポンスを返すこと
       it "responds successfully" do
         sign_in @user
-        get :new
-        expect(response).to be_successful
-      end
-
-      # 200レスポンスを返すこと
-      it "returns a 200 response" do
-        sign_in @user
-        get :new
-        expect(response).to have_http_status "200"
+        get :index
+        aggregate_failures do
+          expect(response).to be_successful
+          expect(response).to have_http_status "200"
+        end
       end
     end
 

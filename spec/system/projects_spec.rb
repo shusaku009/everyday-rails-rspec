22b359acbbd4b26167de72b7ce80ce0bdd2e1899
shusaku009 <shusaku009@gmail.com>
2023-02-28 15:20:24 +0900

    clean システムスペックをリファクタリングした

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -4,8 +4,11 @@ RSpec.describe "Projects", type: :system do
   # ユーザーは新しいプロジェクトを作成する
   scenario "user creates a new project" do
     user = FactoryBot.create(:user)
+    # この章で独自に定義したログインヘルパーを使う場合
+    # sign_in_ad user
+    # もしくはDeviseが提供しているヘルパーを使う場合
     sign_in user
-
+    
     visit root_path
 
     expect {
@@ -13,11 +16,13 @@ RSpec.describe "Projects", type: :system do
       fill_in "Name", with: "Test Project"
       fill_in "Description", with: "Trying out Capybara"
       click_button "Create Project"
+    }.to change(user.projects, :count).by(1)
 
+    aggregate_failures do
       expect(page).to have_content "Project was successfully created"
       expect(page).to have_content "Test Project"
       expect(page).to have_content "Owner: #{user.name}"
-    }.to change(user.projects, :count).by(1)
+    end
   end
   
   # ログインしていないためテストは失敗する
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,24 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :system do
-  # ユーザーがタスクの状態を切り替える
-  scenario "user toggles a task", js: true do
-    user = FactoryBot.create(:user)
-    project = FactoryBot.create(:project,
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) {
+    FactoryBot.create(:project,
       name: "RSpec tutorial",
       owner: user)
-    task = project.tasks.create!(name: "Finish RSpec tutorial")
-    sign_in_as user
+  }
+  let!(:task) { project.tasks.create!(name: "Finish RSpec tutorial") }
 
-    click_link "RSpec tutorial"
-    check "Finish RSpec tutorial"
+  # ユーザーがタスクの状態を切り替える
+  scenario "user toggles a task", js: true do
+    sign_in user
+    go_to_project "RSpec tutorial"
 
-    expect(page).to have_css "label#task_#{task.id}.completed"
-    expect(task.reload).to be_completed
+    complete_task "Finish RSpec tutorial"
+    expect_complete_task "Finish RSpec tutorial"
 
-    uncheck "Finish RSpec tutorial"
+    undo_complete_task "Finish RSpec tutorial"
+    expect_incomplete_task "Finish RSpec tutorial"
+  end
 
-    expect(page).to_not have_css "label#task_#{task.id}.completed"
-    expect(task.reload).to_not be_completed
+  def go_to_project(name)
+    visit root_path
+    click_link name
+  end
+
+  def complete_task(name)
+    check name
+  end
+
+  def undo_complete_task(name)
+    uncheck name
+  end
+
+  def expect_complete_task(name)
+    aggregate_failures do
+      expect(page).to have_css "label.completed", text: name
+      expect(task.reload).to be_completed
+    end
+
+    def expect_incomplete_task(name)
+      aggregate_failures do
+        expect(page).to_not have_css "label.completed", text: name
+        expect(task.reload).to_not be_completed
+      end
+    end
   end
 end
